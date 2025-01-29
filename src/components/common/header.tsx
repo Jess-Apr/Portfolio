2bba4b27ff47c5d9e45cb74a683d2cfcebda97d8
Jess-Apr <jessie1944@naver.com>
2025-01-29 17:59:36 +0900

    💄 Design: Header 양 옆 패딩 너비 조정

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -9,19 +9,21 @@ export default function Header() {
 
     return (
         <nav className="fixed w-full flex justify-between bg-white bg-opacity-80 text-soft-black py-5 px-20 shadow-md">
-            <span className="text-xl font-bold cursor-default">Jessie's Portfolio</span>
-            <ul className="flex space-x-10 items-center">
-                {tabs.map((tab) => (
-                    <li key={tab.text}>
-                        <Link
-                            href={tab.link}
-                            className="hover:border-b hover:border-b-soft-black transition-colors duration-200 font-normal"
-                        >
-                            {tab.text}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <div className="w-full mx-auto md:px-12 lg:px-20 xl:px-28 flex justify-between items-center py-3">
+                <span className="text-xl font-bold cursor-default">Jessie's Portfolio</span>
+                <ul className="flex space-x-10 items-center">
+                    {tabs.map((tab) => (
+                        <li key={tab.text}>
+                            <Link
+                                href={tab.link}
+                                className="hover:border-b hover:border-b-soft-black transition-colors duration-200 font-normal"
+                            >
+                                {tab.text}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </nav>
     )
 }

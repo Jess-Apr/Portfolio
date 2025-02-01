3ad5657ec46fb213a97ad2bb20becbc98d316a78
Jess-Apr <jessie1944@naver.com>
2025-02-01 15:21:53 +0900

    ♻️ Refactor: About 페이지의 타이틀 컴포넌트 범위 변경

--- src/app/about/AboutTitle.tsx
+++ src/app/about/AboutTitle.tsx
@@ -1,10 +1,23 @@
+import Image from "next/image"
+import ProfileImage from "@assets/images/profile_image.png"
+
 export default function AboutTitle() {
     return (
-        <h1 className="flex flex-col text-5xl font-extralight leading-tight">
-            <span>안녕하세요,</span>
-            <span>
-                프론트엔드 개발자 <span className="font-medium">한재연</span>입니다.
-            </span>
-        </h1>
+        <div className="flex justify-center items-center gap-10">
+            <div className="relative w-52 h-52 rounded-full overflow-hidden shadow-lg">
+                <Image
+                    src={ProfileImage}
+                    alt="아이디어를 떠올린 모습의 아바타"
+                    className="absolute w-60 h-60 object-none"
+                    style={{ top: "-8px", left: "-10px" }}
+                />
+            </div>
+            <h1 className="flex flex-col text-5xl font-extralight leading-tight">
+                <span>안녕하세요,</span>
+                <span>
+                    프론트엔드 개발자 <span className="font-medium">한재연</span>입니다.
+                </span>
+            </h1>
+        </div>
     )
 }

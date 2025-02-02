e984c956d05e2c92c44a6f655dd1791147ed0f88
Jess-Apr <jessie1944@naver.com>
2025-02-02 16:39:14 +0900

    💄 Design: 페이지 패딩과 Header 컴포넌트 패딩 통일

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -8,8 +8,8 @@ export default function Header() {
     ]
 
     return (
-        <nav className="fixed w-full flex justify-center bg-white bg-opacity-80 text-soft-black py-5 shadow-md z-10">
-            <div className="w-full flex px-6 sm:px-12 justify-between items-center max-w-6xl">
+        <nav className="fixed w-full flex justify-center bg-white bg-opacity-80 text-soft-black py-5 shadow-md z-10 px-6 sm:px-12">
+            <div className="w-full flex justify-between items-center max-w-6xl">
                 <span className="text-xl font-bold cursor-default">Jessie's Portfolio</span>
                 <ul className="flex space-x-10 items-center">
                     {tabs.map((tab) => (

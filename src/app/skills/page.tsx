b55358eac95ac737edb92f06c150191609a24293
Jess-Apr <jessie1944@naver.com>
2025-02-02 22:51:14 +0900

    💄 Design: Skills 페이지의 hover 액션 삭제

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -6,10 +6,7 @@ export default function SkillsPage() {
             <h2 className="text-3xl font-bold text-black text-center mb-10">Skills</h2>
             <div className="max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-6">
                 {skillsText.map((skill, index) => (
-                    <div
-                        key={index}
-                        className="flex items-start bg-gray-100 p-5 rounded-lg shadow-md transition-transform hover:scale-105"
-                    >
+                    <div key={index} className="flex items-start bg-gray-100 p-5 rounded-lg shadow-md">
                         <img src={skill.icon} alt={skill.name} width={50} height={50} />
                         <div className="ml-4">
                             <span className="block text-lg font-bold text-gray-800">{skill.name}</span>

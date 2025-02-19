25a06c424a233f8b723b8a910c3539367c25467d
Jess-Apr <jessie1944@naver.com>
2025-02-19 22:47:17 +0900

    ✨ Feat: Site Scoop, PinnFlag, DayGrid 프로젝트 데이터 추가

--- src/data/project-details-data-mapper.ts
+++ src/data/project-details-data-mapper.ts
@@ -1,5 +1,8 @@
-import { lingpick, ProjectDetail } from "./project-details"
+import { daygrid, lingpick, pinnflag, ProjectDetail, sitescoop } from "./project-details"
 
 export const projectDataMapper: Record<string, ProjectDetail> = {
+    sitescoop: sitescoop,
     lingpick: lingpick,
+    pinnflag: pinnflag,
+    daygrid: daygrid,
 }
--- src/data/project-details.ts
+++ src/data/project-details.ts
@@ -17,11 +17,31 @@ export type ProjectDetail = {
     images: string[]
 }
 
+export const sitescoop: ProjectDetail = {
+    title: "Site Scoop",
+    info: {
+        desc: "사용자가 지정한 웹 페이지의 테이블을 감지하여 동적 크롤링 후, 데이터를 엑셀 파일로 다운로드할 수 있게 해주는 크롬 확장 프로그램",
+        period: "2025.01 ~",
+        participants: "프론트엔드 1인, 백엔드 1인",
+        role: "프론트엔드 개발",
+    },
+    techStacks: ["JavaScript", "TypeScript", "React", "Jotai", "Tailwind CSS", "Jest"],
+    tasks: [],
+    images: [
+        "/images/sitescoop1.png",
+        "/images/sitescoop2.png",
+        "/images/sitescoop3.png",
+        "/images/sitescoop4.png",
+        "/images/sitescoop5.png",
+        "/images/sitescoop6.png",
+    ],
+}
+
 export const lingpick: ProjectDetail = {
     title: "Lingpick",
     info: {
         desc: "여러 웹사이트와 앱의 링크를 모아서 저장/관리할 수 있고, 인공지능이 저장된 링크를 분석하여 관련 상품을 추천해주는 모바일 앱",
-        period: "2024.08 - 2025.01",
+        period: "2024.08 ~ 2025.01",
         participants: "프론트엔드 1인, 백엔드 1인",
         role: "프론트엔드 개발",
     },
@@ -157,5 +177,256 @@ export const lingpick: ProjectDetail = {
         "/images/lingpick4.png",
         "/images/lingpick5.png",
         "/images/lingpick6.png",
+    ],
+}
+
+export const pinnflag: ProjectDetail = {
+    title: "PinnFlag",
+    info: {
+        desc: "외국인들에게 한국 현지 가이드의 여행상품을 판매하고, 여행 정보를 공유할 수 있는 커뮤니티를 제공해주는 플랫폼",
+        period: "2024.03 ~",
+        participants: "프론트엔드 1인, 백엔드 1인",
+        role: "프론트엔드 개발",
+    },
+    techStacks: ["TypeScript", "React", "Recoil", "Styled-components", "Jest"],
+    tasks: [
+        {
+            title: "<strong>프론트엔드 개발 전반을 주도</strong>하며 작업을 수행",
+            subtasks: [
+                {
+                    title: "커뮤니티 게시판, 여행상품 판매 페이지, 관리자 페이지 등 약 30여 개의 화면을 설계 및 구현",
+                },
+                {
+                    title: "회원가입/로그인, 게시글 및 댓글 CRUD, 카테고리 조회 등 주요 기능에 대한 백엔드 API 연동 작업 진행",
+                },
+                {
+                    title: "클라이언트와 직접 소통하며 요구 사항을 반영하고, UI/UX 개선을 제안 및 적용하여 사용자 경험 최적화",
+                },
+            ],
+        },
+        {
+            title: "<strong>관심사 분리 원칙</strong>을 고려한 구조적 설계",
+            subtasks: [
+                {
+                    title: "MVVM 패턴을 적용하여 뷰 로직과 비즈니스 로직을 분리",
+                    subtasks: [
+                        {
+                            title: "Model: 데이터를 가공하는 비즈니스 로직 처리",
+                        },
+                        {
+                            title: "ViewModel: Model에서 가공된 데이터를 View에 전달하고, API 호출과 상태 관리 수행",
+                        },
+                        {
+                            title: "View: UI를 구성하고 ViewModel로부터 받은 데이터를 표시하며, 사용자의 입력을 ViewModel로 전달",
+                        },
+                    ],
+                },
+                {
+                    title: "각 파일은 300줄 이하, 함수는 20줄 이하로 제한하여 코드 가독성을 높이고, 책임 분리를 명시적으로 적용",
+                },
+            ],
+        },
+        {
+            title: "Jest를 사용하여 ViewModel과 Model 레이어의 <strong>단위 테스트 수행</strong>",
+        },
+        {
+            title: "i18next 라이브러리를 사용하여 <strong>다국어 지원 기능 제공</strong>",
+            subtasks: [
+                {
+                    title: "각 언어에 대응하는 키-언어 쌍을 가진 JSON 번역 파일을 기반으로 동적으로 콘텐츠를 렌더링할 수 있도록 설계",
+                },
+                {
+                    title: "언어 전환 시에도 일관된 UI/UX를 제공하여 글로벌 사용자 경험 최적화",
+                },
+            ],
+        },
+        {
+            title: "개발자가 아닌 클라이언트와의 <strong>원활한 소통을 위한 시각적 자료 제작</strong>",
+            subtasks: [
+                {
+                    title: "UI 수정을 제안할 때 Figma로 수정 사항이 적용된 화면을 그린 후 제안 사항과 함께 전달",
+                },
+                {
+                    title: "복잡한 코드의 흐름을 플로우 차트로 제작하여 코드를 더욱 쉽게 이해할 수 있도록 함",
+                },
+            ],
+        },
+        {
+            title: "효율적인 협업을 위한 <strong>작업 우선순위 문서화 도입 및 적용</strong>",
+            subtasks: [
+                {
+                    title: "클라이언트, 프론트엔드, 백엔드 간 우선순위 불일치로 인한 작업 지연 문제를 해결하기 위해 문서화 프로세스 제안 및 실행",
+                },
+                {
+                    title: "Google Docs를 활용해 구성원들이 각자의 작업 우선순위를 공유하고, 주간 미팅에서 이를 기반으로 작업 순서 조정 및 진행 상황 점검",
+                },
+                {
+                    title: "클라이언트와 약속된 기능의 누락을 줄이고, 개발 진행 속도를 향상시켜 마일스톤 목표를 안정적으로 달성",
+                },
+            ],
+        },
+    ],
+    images: [
+        "/images/pinnflag1.png",
+        "/images/pinnflag2.png",
+        "/images/pinnflag3.png",
+        "/images/pinnflag4.png",
+        "/images/pinnflag5.png",
+        "/images/pinnflag6.png",
+        "/images/pinnflag7.png",
+        "/images/pinnflag8.png",
+    ],
+}
+
+export const daygrid: ProjectDetail = {
+    title: "DayGrid",
+    info: {
+        desc: "달력에 일정을 남기거나 디데이 설정을 하여 일정 관리를 할 수 있고, 원하는 날짜에 다이어리를 작성하여 기록할 수 있는 캘린더 웹 어플리케이션",
+        period: "2023.10 ~ 2023.11",
+        participants: "풀스택 개발 2인",
+        role: "프론트엔드 개발, 백엔드 개발, 프로젝트 기획 및 디자인",
+    },
+    techStacks: ["TypeScript", "React", "Redux Toolkit", "Tanstack Query", "Node.js", "Express", "MySQL"],
+    tasks: [
+        {
+            title: "<strong>Access token</strong>과 <strong>Refresh token<strong>을 활용한 유저 인증 로직 구현",
+            subtasks: [
+                {
+                    title: "Axios 인터셉터를 이용하여 각 요청마다 Access token을 자동 삽입하며, 토큰 만료 시 Refresh token으로 토큰 재발급을 받을 수 있도록 설정",
+                },
+                {
+                    title: "보안 강화를 위한 토큰 설정",
+                    subtasks: [
+                        {
+                            title: "Access token은 외부 접근이 어려운 로컬 변수에, Refresh token은 브라우저에서 접근이 불가능한 HTTP Only Cookies에 저장",
+                        },
+                        {
+                            title: "Access token의 유효기간을 1시간으로 짧게 설정하여, 토큰이 악용될 가능성을 최소화",
+                        },
+                        {
+                            title: "Refresh token은 Access token 만료 시에만 서버로 전송하여 토큰 탈취 위험을 줄임",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: "외부 라이브러리를 사용하지 않고 달력 기능 구현",
+            subtasks: [
+                {
+                    title: "월간 달력은 grid, 주간 달력은 flex를 활용하여 구현",
+                },
+            ],
+        },
+        {
+            title: "TanStack Query를 사용하여 마이페이지의 서버 데이터를 효율적으로 관리",
+            subtasks: [
+                {
+                    title: "쉽게 변하지 않는 유저 정보는 시간 제한 없이 캐싱하여 불필요한 fetching을 방지",
+                },
+                {
+                    title: "사용자 정보가 수정되었을 경우, useMutation을 통해 서버 데이터를 업데이트하고, 캐시 데이터를 무효화하여 서버로부터 최신 데이터를 새로 가져오도록 설정",
+                },
+            ],
+        },
+        {
+            title: "일정을 날짜별로 파악할 수 있는 일정 관리 기능 구현",
+            subtasks: [
+                {
+                    title: "일정을 생성할 경우, 각 일정의 날짜와 시간에 맞추어 달력에 일정 표시가 생성될 수 있도록 설계",
+                },
+                {
+                    title: "일정 표시의 position을 absolute로 설정하고, 날짜와 시간에 따라 left와 top 속성을 조정하여 표시",
+                },
+            ],
+        },
+        {
+            title: "<strong>서버 구축, 데이터베이스 연동 및 API 작성</strong>",
+            subtasks: [
+                {
+                    title: "API 작성 시에는 자원과 행위를 명확하게 표현한 <strong>RESTful API</strong>로 작성",
+                },
+            ],
+        },
+        {
+            title: "CRA를 사용하지 않고, <strong>Webpack과 Babel을 설정</strong>하여 프론트엔드 개발 환경 세팅",
+        },
+    ],
+    improvements: [
+        {
+            title: "달력 일정 카드 UI 개선",
+            subtasks: [
+                {
+                    title: "<strong>문제:</strong> 같은 날짜에 일정을 여러개 등록할 경우, 달력에 생성된 일정 카드가 다른 일정 카드를 가리는 현상 발생",
+                },
+                {
+                    title: "<strong>해결:</strong>",
+                    subtasks: [
+                        {
+                            title: "각 일정 카드의 z-index 값을 시간이 길수록 낮게, 짧을수록 높게 주어 시간이 긴 일정일수록 뒤로 배치",
+                        },
+                        {
+                            title: "일정 시작 시간이 같은 일정이 여러개일 경우, '1/일정갯수'의 길이로 일정 카드 너비 축소",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: "CORS 에러 해결",
+            subtasks: [
+                {
+                    title: "<strong>원인:</strong> CORS 에러는 브라우저의 동일 출처 정책으로 인해 발생. 서버와 브라우저의 출처가 달라 접근이 제한되었음.",
+                },
+                {
+                    title: "<strong>해결:</strong>",
+                    subtasks: [
+                        {
+                            title: "Access-Control-Allow-Origin에 브라우저의 출처를 명시하여 브라우저의 접근을 허용하도록 설정",
+                        },
+                        {
+                            title: "답 헤더의 Access-Control-Allow-Credentials 옵션을 true로 설정하여, 브라우저가 응답에 접근 가능하도록 하였음",
+                        },
+                        {
+                            title: "Access-Controls-Allow-Methods에 브라우저에서 자원을 접근할 때 사용하는 모든 메서드를 명시하여, HTTP 메서드에 상관 없이 접근을 허용",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: "검색 페이지 무한 로딩 문제 해결",
+            subtasks: [
+                {
+                    title: "<strong>문제:</strong> 검색 키워드 입력 후 스크롤 시 Masonry List의 커스텀 태그에서 API 요청 스크롤 이벤트가 중복 발생하여 무한 로딩 현상 발생",
+                },
+                {
+                    title: "<strong>원인:</strong> Masonry List 구조는 열 높이가 달라 onEndReached가 각 열의 끝을 계산하는 과정에서 스크롤 이벤트가 반복적으로 트리거될 수 있음",
+                },
+                {
+                    title: "<strong>해결:</strong>",
+                    subtasks: [
+                        {
+                            title: "Debouncing 함수를 구현하여 마지막 스크롤 이벤트 발생 후 200ms가 지나야 API 요청을 보내도록 설정",
+                        },
+                        {
+                            title: "useInfiniteQuery의 isLoading 상태를 감지하여 로딩 중에는 추가 API 요청을 차단",
+                        },
+                    ],
+                },
+                {
+                    title: "<strong>성과:</strong> 중복 API 요청을 방지하고 무한 로딩 문제를 해결해 검색 페이지의 안정성과 성능 개선",
+                },
+            ],
+        },
+    ],
+    images: [
+        "/images/daygrid1.png",
+        "/images/daygrid2.png",
+        "/images/daygrid3.png",
+        "/images/daygrid4.png",
+        "/images/daygrid5.png",
+        "/images/daygrid6.png",
+        "/images/daygrid7.png",
     ],
 }
